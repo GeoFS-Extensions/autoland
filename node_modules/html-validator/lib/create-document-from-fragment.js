be9e61bd59d10b7c2955f633b12f2cd9a397b5b9
nicolas377 <programnicolas@gmail.com>
2021-07-27 14:39:57 -0400

    add even more stuff

--- node_modules/html-validator/lib/create-document-from-fragment.js
+++
@@ -1,1 +0,0 @@
-module.exports = fragment => `<!DOCTYPE html><html lang="en"><head><title>Document from fragment</title></head><body>${fragment}</body></html>`
--- test.js
+++ test.js
@@ -1,4 +1,30 @@
-const htmllint = require('html-validator')
-const validateCss = require('css-validator');
-var LintStream = require('jslint').LintStream;
+const fs = require('fs')
+const glob = require("glob")
 
+/**
+ * Gets all the files in the build dir with a certain extension
+ * @param {string} extension file extension to look for
+ * @returns {Promise<string[]>} array of files
+ */
+async function getFiles(extension) {
+	return new Promise((resolve, reject) => {
+		glob(`build/**/*.${extension}`, function (er, files) {
+			if (er) {
+				reject(er)
+			}
+			resolve(files)
+		})
+	})
+}
+
+// lint js
+(async function() {
+	var toLint = await getFiles('js')
+
+	toLint.forEach(async (element) => {
+		var file = await fs.readFileSync(element, {
+			encoding: 'utf8'
+		})
+		
+	});
+})()
