cc44f2e497dfa8087809882b9656bbbf31fadad2
nicolas377 <programnicolas@gmail.com>
2021-07-27 08:41:04 -0400

    Popup, service worker, ap++ and fmc (#3)
    * keep working on background script
    
    * Build extension
    
    * keep working on background
    
    * Build extension
    
    * done for today
    
    * Build extension
    
    * keep working on background and popup
    
    Co-authored-by: Guy Adler <Guy-Adler@users.noreply.github.com>
    
    * Build extension
    
    * still working on extension
    
    Co-authored-by: Guy Adler <Guy-Adler@users.noreply.github.com>
    
    * Build extension
    
    * done with popup
    
    Co-authored-by: Guy Adler <Guy-Adler@users.noreply.github.com>
    
    * working on a local build
    
    * keep working on build function
    
    * still working
    
    * done with the npm build action
    
    Co-authored-by: github-actions <action@github.com>
    Co-authored-by: Guy Adler <Guy-Adler@users.noreply.github.com>

---
+++ build.js
@@ -0,0 +1,33 @@
+// copy extension to build env
+import * as fs from 'fs'
+import * as fse from 'fs-extra'
+import pkg from 'async';
+const { series } = pkg;
+import pkhg from 'child_process'
+const { exec } = pkhg
+
+function deleteFolderRecursive(path) {
+	if (fs.existsSync(path) && fs.lstatSync(path).isDirectory()) {
+		fs.readdirSync(path).forEach(function (file, index) {
+			var curPath = path + "/" + file;
+
+			if (fs.lstatSync(curPath).isDirectory()) { // recurse
+				deleteFolderRecursive(curPath);
+			} else { // delete file
+				fs.unlinkSync(curPath);
+			}
+		});
+
+		fs.rmdirSync(path);
+	}
+};
+
+deleteFolderRecursive('build')
+
+fse.copySync('extension/', 'build')
+
+series([
+	() => exec('npx tsc')
+])
+
+// TODO: delete the .ts files in build dir
