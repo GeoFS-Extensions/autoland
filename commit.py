f0c9c8c986ce01fd81b71250d9410576a1130da2
nicolas377 <programnicolas@gmail.com>
2021-08-03 13:06:02 -0400

    run commit.py from npm

--- commit.py
+++ commit.py
@@ -1,9 +1,13 @@
 import os
 
+# Get the commit name
 name = input('Enter commit name\n> ')
 
+# build the extension
 os.system('npm run build')
+
+# commit
 os.system('git pull')
 os.system('git add .')
-os.system(f'git commit -m "${name}" -a --allow-empty')
+os.system(f'git commit -m "{name}" -a --allow-empty')
 os.system('git push')
