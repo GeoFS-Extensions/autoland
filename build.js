7129e987dc9ec57d8381be7862264be812eb7425
nicolas377 <programnicolas@gmail.com>
2021-09-04 11:00:53 -0400

    got rid of npm dependency, closes #46

--- build.js
+++ build.js
@@ -2,8 +2,6 @@
 /* eslint @typescript-eslint/no-var-requires: 0*/
 
 const fs = require("fs-extra");
-const path = require("path");
-const npm = require("npm");
 
 function deleteFolderRecursive(path) {
   if (fs.existsSync(path) && fs.lstatSync(path).isDirectory()) {
@@ -23,44 +21,8 @@ function deleteFolderRecursive(path) {
   }
 }
 
-function deleteTsFiles(
-  startPath = "./build",
-  filter = ".ts",
-  notRecursing = true
-) {
-  if (notRecursing) {
-    console.log(".ts files compiled!");
-  }
-
-  if (!fs.existsSync(startPath)) {
-    console.log("no dir ", startPath);
-    return;
-  }
-
-  var files = fs.readdirSync(startPath);
-  for (var i = 0; i < files.length; i++) {
-    var filename = path.join(startPath, files[i]);
-    var stat = fs.lstatSync(filename);
-    if (stat.isDirectory()) {
-      deleteTsFiles(filename, filter, false); // recurse
-    } else if (filename.indexOf(filter) >= 0) {
-      fs.unlinkSync(filename);
-    }
-  }
-
-  if (notRecursing) {
-    console.log(".ts files deleted!");
-  }
-}
-
 deleteFolderRecursive("build");
 console.log("build folder deleted!");
 
 fs.copySync("source", "build");
 console.log("source copied to build!");
-
-npm.load(function () {
-  npm.commands["run-script"](["tsc"], () => {
-    deleteTsFiles();
-  });
-});
