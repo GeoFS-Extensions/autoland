7dedeca5acf9bd799e62ef06ccbb222636689377
nicolas377 <programnicolas@gmail.com>
2021-09-05 10:15:42 -0400

    bring in ap++, build data nightly

--- build.js
+++ build.js
@@ -3,6 +3,28 @@
 
 const fs = require("fs-extra");
 
+// build airports.json
+function airportsList() {
+  let a = fs.readFileSync("./data/airports.csv", { encoding: "utf8" });
+  a = a.split("\n");
+  a.shift();
+
+  const airports = {};
+
+  for (let airport of a) {
+    if (airport[0] == undefined) continue;
+    airport = airport.split(",");
+
+    let name = airport[1].substring(1).substring(0, airport[1].length - 2);
+    airports[name] = [Number(airport[4]), Number(airport[5])];
+  }
+
+  fs.writeFileSync(
+    "./source/data/airports.json",
+    JSON.stringify(airports, null, 2)
+  );
+}
+
 function deleteFolderRecursive(path) {
   if (fs.existsSync(path) && fs.lstatSync(path).isDirectory()) {
     fs.readdirSync(path).forEach(function (file) {
@@ -20,6 +42,9 @@ function deleteFolderRecursive(path) {
     fs.rmdirSync(path);
   }
 }
+
+airportsList();
+console.log("airport list built!");
 
 deleteFolderRecursive("build");
 console.log("build folder deleted!");
