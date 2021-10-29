5335aa13e1637821e064c0a1088f04c616f6b813
Guy Adler <guyadler2005@gmail.com>
2021-10-29 14:58:19 +0300

    started converting to webapck

--- autopilot_pp/init.js
+++ autopilot_pp/init.js
@@ -1,34 +1,26 @@
-/*!
- * @license Copyright (c) Karl Cheng 2013-17
- * Licensed under the GNU General Public Licence, version 3 or later.
- * See the LICENSE.md file for details.
- */
 "use strict";
-// Make sure code is run after GEFS is ready.
-(function () {
-  function checkKeyboardMapping() {
-    var _a = window.keyboard_mapping;
-    return _a === null || _a === void 0 ? false : _a.ready;
-  }
+function checkKeyboardMapping() {
+  var _a = window.keyboard_mapping;
+  return _a === null || _a === void 0 ? false : _a.ready;
+}
 
-  function load() {
-    require(["./build/ui/main"]);
-  }
+function load() {
+  require(["./build/ui/main"]);
+}
 
-  var timer = setInterval(function () {
-    if (
-      !(
-        window.geofs &&
-        window.geofs.canvas &&
-        window.geofs.aircraft &&
-        window.geofs.aircraft.instance &&
-        window.geofs.aircraft.instance.object3d &&
-        window.navData.statusCode === 1 &&
-        checkKeyboardMapping()
-      )
+var timer = setInterval(function () {
+  if (
+    !(
+      window.geofs &&
+      window.geofs.canvas &&
+      window.geofs.aircraft &&
+      window.geofs.aircraft.instance &&
+      window.geofs.aircraft.instance.object3d &&
+      window.navData.statusCode === 1 &&
+      checkKeyboardMapping()
     )
-      return;
-    clearInterval(timer);
-    load();
-  }, 250);
-})();
+  )
+    return;
+  clearInterval(timer);
+  load();
+}, 250);
