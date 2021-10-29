36f9ece7f18986d00c0c8b2a46fc9adc74a10a71
Guy Adler <guyadler2005@gmail.com>
2021-10-29 19:08:29 +0300

    Updated spoilers_arming_build to support webpack

---
+++ infra/builds/spoilers_arming_build.js
@@ -0,0 +1,96 @@
+const { execSync } = require("child_process");
+const { join } = require("path");
+const { chdir } = require("process");
+const homeDir = require("../main_dir");
+const webpack = require("webpack");
+const chalk = require("chalk");
+
+function scriptTag() {
+  return chalk.hex("#55e09d")("(spoilers_arming) ");
+}
+
+/**
+ * Builds spoilers arming.
+ * @param {boolean} debug Whether the script should be built for debugging.
+ */
+async function build(debug) {
+  // if we want to build the script for debugging
+  if (!debug) {
+    console.log(
+      scriptTag() +
+        chalk.hex("#f573a3")("Debug set to false, applying uglify configs...")
+    );
+  }
+
+  // change dirs to the script dir
+  const scriptLocation = join(homeDir, "spoilers_arming");
+  chdir(scriptLocation);
+
+  // run the typescript compiler in a child process
+  try {
+    console.log(
+      scriptTag() +
+        chalk.hex("#b1c6fc")(
+          "Running the typescript compiler in a child process..."
+        )
+    );
+    execSync("npx tsc");
+  } catch (e) {
+    throw new Error(
+      scriptTag() +
+        chalk.hex("#ff0000")(`Compiling failed with exit code `) +
+        chalk.hex("#fff200")(`${e.code}.\n\n`) +
+        e.stdout.toString()
+    );
+  }
+
+  // optimize the requirejs file
+  const compiler = webpack({
+    entry: join(homeDir, "spoilers_arming/init.js"),
+    mode: !debug ? "production" : "development",
+    output: {
+      path: join(homeDir, "extension/source/scripts"),
+      filename: "spoilers_arming.js",
+    },
+  });
+  return new Promise((resolve, reject) => {
+    try {
+      compiler.run((err, stats) => {
+        if (err || stats.hasErrors) {
+          if (err) {
+            reject(err.stack || err);
+            if (err.details) {
+              reject(err.details);
+            }
+            return;
+          }
+    
+          const info = stats.toJson();
+    
+          if (stats.hasErrors()) {
+            reject(info.errors);
+          }
+        
+          if (stats.hasWarnings()) {
+            console.warn(info.warnings);
+          }
+    
+          if (!(stats.hasWarnings() || stats.hasErrors())) {
+            console.log(stats.toString({colors: true}));
+          }
+        }
+        
+        compiler.close(closeErr => {
+          if (closeErr) {
+            reject(closeErr);
+          }
+          resolve();
+        });
+      });
+    } catch (err) {
+      reject(err);
+    }
+  });
+}
+
+module.exports = build;
