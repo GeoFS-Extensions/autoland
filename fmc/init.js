f921909f5aba61976cb0386a5fd47dd07b8b5d41
Guy Adler <guyadler2005@gmail.com>
2021-10-30 15:15:14 +0300

    fixed some globals

--- fmc/init.js
+++ fmc/init.js
@@ -23,6 +23,5 @@ var timer = setInterval(function () {
   window.fmc.version = "0.6.0";
   window.fmc.ready = false;
   require("./source/ui/main");
-  /* global __webpack_require__ */ // Added by webpack.
   window.fmc.require = __webpack_require__;
 }, 250);
